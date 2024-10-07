10296192001e35d0afa0b4546fcdd0ff3537153a
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-10-07 18:18:36 +0700

    Initial Commit

---
+++ CoffeeShopApp/CoffeeShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopAppApp.swift
+//  CoffeeShopApp
+//
+//  Created by Ricky Primayuda Putra on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeShopAppTests/CoffeeShopAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopAppTests.swift
+//  CoffeeShopAppTests
+//
+//  Created by Ricky Primayuda Putra on 07/10/24.
+//
+
+import Testing
+@testable import CoffeeShopApp
+
+struct CoffeeShopAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
